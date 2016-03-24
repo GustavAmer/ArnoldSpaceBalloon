92444b8d66bc0b2828a0b1a7ff7efc1a81cf3b9f
Gustav Amer <gustav.amer@rgrd.ee>
2016-03-24 15:06:17 +0200

    Init

---
+++ app/src/main/java/com/example/gustav/arnoldspaceballoon/SpaceBalloonActivity.java
@@ -0,0 +1,66 @@
+package com.example.gustav.arnoldspaceballoon;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class SpaceBalloonActivity extends AppCompatActivity {
+
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_balloon);
+        tv = (TextView)(findViewById(R.id.text));
+        LocationManager lm = (LocationManager)(this.getSystemService(Context.LOCATION_SERVICE));
+        boolean enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+                double alt = location.getAltitude();
+                tv.setText(lat + " " + lng + " "  + alt);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        try {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }catch (SecurityException e){}
+
+        // Create an instance of Camera
+        Camera mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        CameraPreview mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+}
